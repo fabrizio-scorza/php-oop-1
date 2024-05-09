e11a7524d8b6562d76f9b9b5d116c4bd130f6c62
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-09 16:47:54 +0200

    FEAT html and php class istances

--- index.php
+++ index.php
@@ -34,6 +34,9 @@ class Production
 $x_man = new Production('X Man', 'en', '8');
 $acquaman = new Production('Acquaman', 'en', '6');
 
+//creo un array con le istanze create
+$films = [$x_man, $acquaman];
+
 ?>
 
 <!DOCTYPE html>
@@ -46,23 +49,29 @@ $acquaman = new Production('Acquaman', 'en', '6');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
-<body>
+<body class="bg-dark">
     <main>
         <div class="p-3 mt-5 container">
             <div class="row row-cols-4">
-                <div class="col">
-                    <div class="card text-center">
-                        <h2><?php echo $x_man->title ?></h2>
-                        <p>
-                            <span>
-                                <?php echo $x_man->lang ?>
-                            </span>
-                            <span>
-                                <?php echo $x_man->vote ?>
-                            </span>
-                        </p>
+                <?php
+                foreach ($films as $film) {
+                ?>
+                    <div class="col">
+                        <div class="card text-center">
+                            <h2><?php echo $film->title ?></h2>
+                            <p>
+                                <span>
+                                    <?php echo $film->lang ?>
+                                </span>
+                                <span>
+                                    <?php echo $film->vote ?>
+                                </span>
+                            </p>
+                        </div>
                     </div>
-                </div>
+                <?php
+                }
+                ?>
             </div>
         </div>
     </main>
